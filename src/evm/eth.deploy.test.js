5aa6f3f3be9ead69b2a0d304c313b0c8399e3a6f
open-junius <zhou@opentensor.dev>
2024-11-15 09:18:44 +0800

    add not whitelist test case

--- src/evm/eth.deploy.test.js
+++ src/evm/eth.deploy.test.js
@@ -25,6 +25,37 @@ const amount1ETH = convertEtherToWei(1.0);
 let fundedEthWallet = generateRandomAddress();
 let ed;
 
+const byteCode =
+  "0x6080604052348015600e575f80fd5b506101438061001c5f395ff3fe608060405234801561000f575f80fd5b5060043610610034575f3560e01c80632e64cec1146100385780636057361d14610056575b5f80fd5b610040610072565b60405161004d919061009b565b60405180910390f35b610070600480360381019061006b91906100e2565b61007a565b005b5f8054905090565b805f8190555050565b5f819050919050565b61009581610083565b82525050565b5f6020820190506100ae5f83018461008c565b92915050565b5f80fd5b6100c181610083565b81146100cb575f80fd5b50565b5f813590506100dc816100b8565b92915050565b5f602082840312156100f7576100f66100b4565b5b5f610104848285016100ce565b9150509291505056fea26469706673582212209a0dd35336aff1eb3eeb11db76aa60a1427a12c1b92f945ea8c8d1dfa337cf2264736f6c634300081a0033";
+const abi = [
+  {
+    inputs: [],
+    name: "retrieve",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "num",
+        type: "uint256",
+      },
+    ],
+    name: "store",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
 describe("Smart contract deployment", () => {
   before(async () => {
     await usingApi(async (api) => {
@@ -47,55 +78,35 @@ describe("Smart contract deployment", () => {
         amount1TAO.multipliedBy(1000).toString()
       );
       await sendTransaction(api, transfer, tk.alice);
+    });
+  });
 
-      // white list the contract creator
-      const whiteList = api.tx.evm.setWhitelist([fundedEthWallet.address]);
-      const sudoCall = api.tx.sudo.sudo(whiteList);
-      await sendTransaction(api, sudoCall, tk.alice);
+  it("Can't deploy a smart contract since the account not in white list", async () => {
+    await usingEthApi(async (provider) => {
+      const signer = new ethers.Wallet(fundedEthWallet.privateKey, provider);
+      const contractFactory = new ethers.ContractFactory(abi, byteCode, signer);
+
+      try {
+        const contract = await contractFactory.deploy();
+        await contract.waitForDeployment();
+      } catch (error) {
+        expect(error.toString().includes("Error"));
+      }
     });
   });
 
   it("Can deploy a smart contract", async () => {
     await usingEthApi(async (provider) => {
-      const byteCode =
-        "0x6080604052348015600e575f80fd5b506101438061001c5f395ff3fe608060405234801561000f575f80fd5b5060043610610034575f3560e01c80632e64cec1146100385780636057361d14610056575b5f80fd5b610040610072565b60405161004d919061009b565b60405180910390f35b610070600480360381019061006b91906100e2565b61007a565b005b5f8054905090565b805f8190555050565b5f819050919050565b61009581610083565b82525050565b5f6020820190506100ae5f83018461008c565b92915050565b5f80fd5b6100c181610083565b81146100cb575f80fd5b50565b5f813590506100dc816100b8565b92915050565b5f602082840312156100f7576100f66100b4565b5b5f610104848285016100ce565b9150509291505056fea26469706673582212209a0dd35336aff1eb3eeb11db76aa60a1427a12c1b92f945ea8c8d1dfa337cf2264736f6c634300081a0033";
-      const abi = [
-        {
-          inputs: [],
-          name: "retrieve",
-          outputs: [
-            {
-              internalType: "uint256",
-              name: "",
-              type: "uint256",
-            },
-          ],
-          stateMutability: "view",
-          type: "function",
-        },
-        {
-          inputs: [
-            {
-              internalType: "uint256",
-              name: "num",
-              type: "uint256",
-            },
-          ],
-          name: "store",
-          outputs: [],
-          stateMutability: "nonpayable",
-          type: "function",
-        },
-      ];
-
       const signer = new ethers.Wallet(fundedEthWallet.privateKey, provider);
+      await usingApi(async (api) => {
+        // Alice gives permission to signer to create a contract
+        const txSudoSetWhitelist = api.tx.sudo.sudo(
+          api.tx.evm.setWhitelist([signer.address])
+        );
 
-      // Alice gives permission to signer to create a contract
-      const txSudoSetWhitelist = api.tx.sudo.sudo(
-        api.tx.evm.setWhitelist([signer.address])
-      );
+        await sendTransaction(api, txSudoSetWhitelist, tk.alice);
+      });
 
-      await sendTransaction(api, txSudoSetWhitelist, tk.alice);
       const contractFactory = new ethers.ContractFactory(abi, byteCode, signer);
       const contract = await contractFactory.deploy();
 
