0916d092f181f3cc982e047b84a1d330daea822c
open-junius <zhou@opentensor.dev>
2025-01-22 20:25:02 +0800

    update data for certificate

--- src/evm/eth.serve.axon-prometheus.test.js
+++ src/evm/eth.serve.axon-prometheus.test.js
@@ -109,7 +109,7 @@ describe("Serve Axon Prometheus test", () => {
       const ipType = 4;
       const protocol = 0;
       const placeholder1 = 8;
-      const placeholder2 = 0;
+      const placeholder2 = 9;
 
       const signer = new ethers.Wallet(fundedEthWallet1.privateKey, provider);
       const contract = new ethers.Contract(INEURON_ADDRESS, INeuronABI, signer);
@@ -137,8 +137,14 @@ describe("Serve Axon Prometheus test", () => {
       const ipType = 4;
       const protocol = 0;
       const placeholder1 = 8;
-      const placeholder2 = 0;
-      const certificate = new Uint8Array(65);
+      const placeholder2 = 9;
+      // certificate length is 65
+      const certificate = new Uint8Array([
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+        39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
+        57, 58, 59, 60, 61, 62, 63, 64, 65,
+      ]);
 
       const signer = new ethers.Wallet(fundedEthWallet2.privateKey, provider);
       const contract = new ethers.Contract(INEURON_ADDRESS, INeuronABI, signer);
