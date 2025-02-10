972dd0d405a0234d25d8d05bfef219b348f998a7
open-junius <zhou@opentensor.dev>
2025-02-10 23:58:34 +0800

    set rate limit before all test

--- src/setup.js
+++ src/setup.js
@@ -1,21 +1,27 @@
 import { WsProvider, ApiPromise } from "@polkadot/api";
-import { WS_ENDPOINT, CONN_TIMEOUT } from '../config.js';
+import { WS_ENDPOINT, CONN_TIMEOUT } from "../config.js";
+import { getTestKeys } from "./util/known-keys.js";
 
 function withTimeout(promise, timeoutMs) {
   // Create a promise that rejects in <timeoutMs> milliseconds
   let timeoutHandle;
   const timeoutPromise = new Promise((resolve, reject) => {
-      timeoutHandle = setTimeout(() => reject(new Error('Promise timed out')), timeoutMs);
+    timeoutHandle = setTimeout(
+      () => reject(new Error("Promise timed out")),
+      timeoutMs
+    );
   });
 
   // Returns a race between our timeout and the passed in promise
-  return Promise.race([promise, timeoutPromise]).then(result => {
+  return Promise.race([promise, timeoutPromise])
+    .then((result) => {
       clearTimeout(timeoutHandle);
       return result;
-  }).catch(error => {
+    })
+    .catch((error) => {
       clearTimeout(timeoutHandle);
       throw error;
-  });
+    });
 }
 
 export async function createApi() {
@@ -25,24 +31,32 @@ export async function createApi() {
     await withTimeout(api.isReady, CONN_TIMEOUT);
   } catch (error) {
     global.api.disconnect();
-    throw Error('Connection timeout')
+    throw Error("Connection timeout");
   }
 
   const cleanup = () => {
-    global.api.off('disconnected');
-    global.api.off('error');
+    global.api.off("disconnected");
+    global.api.off("error");
   };
 
-  global.api.on('disconnected', cleanup);
-  global.api.on('error', cleanup);
+  global.api.on("disconnected", cleanup);
+  global.api.on("error", cleanup);
 
   return global.api;
 }
 
 before(async () => {
-  await createApi();
+  let api = await createApi();
+  let rateLimit = Number(await api.query.subtensorModule.networkRateLimit());
+  if (rateLimit > 0) {
+    const tk = getTestKeys();
+    const txSudoSetNetworkRateLimit = api.tx.sudo.sudo(
+      api.tx.adminUtils.sudoSetNetworkRateLimit(0)
+    );
+    await sendTransaction(api, txSudoSetNetworkRateLimit, tk.alice);
+  }
 });
 
 after(() => {
-  global.api && global.api.disconnect()
+  global.api && global.api.disconnect();
 });
